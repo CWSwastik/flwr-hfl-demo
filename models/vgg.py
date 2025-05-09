a6248373e671cd29fb0686e650358a433a62a939
Swastik Goswami <cwswastik2005@gmail.com>
2025-05-09 19:20:43 +0530

    fix classes

--- models/vgg.py
+++ models/vgg.py
@@ -8,10 +8,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.base_model = vgg11()
 
-        # self.base_model.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        # # self.base_model.classifier = nn.Sequential(
-        # #     nn.Linear(512, 256), nn.ReLU(), nn.Dropout(0.3), nn.Linear(256, num_classes)
-        # # )
+        self.base_model.classifier[6] = nn.Linear(4096, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.base_model(x)
