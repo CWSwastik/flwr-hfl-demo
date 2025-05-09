a18152eded5e4e84dfd84a8dfb214e132f3c6349
Swastik Goswami <cwswastik2005@gmail.com>
2025-05-09 19:04:30 +0530

    fix: use base vgg11

--- models/vgg.py
+++ models/vgg.py
@@ -6,13 +6,12 @@ from torchvision.models import vgg11
 class Net(nn.Module):
     def __init__(self, num_classes=10):
         super(Net, self).__init__()
-        # Load the VGG11 architecture without pretrained weights
         self.base_model = vgg11()
 
-        self.base_model.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # Keep spatial dims
-        self.base_model.classifier = nn.Sequential(
-            nn.Linear(512, 256), nn.ReLU(), nn.Dropout(0.3), nn.Linear(256, num_classes)
-        )
+        # self.base_model.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        # # self.base_model.classifier = nn.Sequential(
+        # #     nn.Linear(512, 256), nn.ReLU(), nn.Dropout(0.3), nn.Linear(256, num_classes)
+        # # )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.base_model(x)
