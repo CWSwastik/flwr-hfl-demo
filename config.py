2e9b5d8bb1a29f00cf1337b40f7375fa2ab85a9e
Swastik Goswami <cwswastik2005@gmail.com>
2025-09-04 08:57:41 +0530

    feat: vgg

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 NUM_ROUNDS = 100
-TOPOLOGY_FILE = "topo.yml"
-NUM_CLIENTS = 4
+TOPOLOGY_FILE = "topo-10c.yml"
+
+NUM_CLIENTS = 10
+MIN_CLIENTS_PER_EDGE = 3
 
 MODEL = "lenet_mnist"  # vgg, lenet_mnist, lenet_cifar10
 DATASET = "fashion_mnist"  # mnist, cifar10, fashion_mnist
@@ -15,4 +17,4 @@ TRAINING_WEIGHT_DECAY = 1e-4
 TRAINING_SCHEDULER_STEP_SIZE = 10
 TRAINING_SCHEDULER_GAMMA = 0.1
 
-DASHBOARD_SERVER_URL = "https://f64e2d22cc67.ngrok-free.app"
+DASHBOARD_SERVER_URL = "https://f772957a48fe.ngrok-free.app"
